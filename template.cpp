0ab8e322576f9395626a2c6b1eba02707a026203
Wojciech Baranowski <59918953+SimmonDobber@users.noreply.github.com>
2023-01-12 23:41:28 +0100

    Add files via upload

---
+++ template.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+ 
+#define INF 1000000311
+#define LINF 1000000000000000311
+#define M 1000000007
+#define M1 998244353
+ 
+using namespace std;
+using ll=long long;
+using pi=pair<int, int>;
+using pl=pair<ll, ll>;
+using vi=vector<int>;
+using vl=vector<ll>;
+using gr=vector<vector<int> >;
+ 
+#define fp(x, a, b) for (int (x) = (a); (x) < (b); (x)++)
+#define f(x, n) for (int (x) = 0; (x) < (n); (x)++)
+#define fnp(x, a, b) for (int (x) = (b) - 1; (x) >= (a); (x)--)
+#define fn(x, n) for (int (x) = (n - 1); (x) >= 0; (x)--)
+#define gcd(a, b) __gcd((a), (b))
+#define lcm(a, b) (a) * (b) / gcd((a), (b))
+#define x first
+#define y second
+#define mp make_pair 
+#define pb push_back
+#define s(x) (int)x.size()
+#define all(x) x.begin(), x.end()
+#define ans(x) cout<<(x)<<"\n"
+#define yes cout<<"YES\n";
+#define no cout<<"NO\n";
+#define nl "\n"
+#define fl cout.flush()
+
+mt19937 rnd(chrono::high_resolution_clock::now().time_since_epoch().count());
+
+int T = 1; 
+void preprocess();
+void input();
+void compute();
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	cout << fixed << setprecision(15);
+	preprocess();
+	cin >> T;
+	while(T--) {
+		input();
+		compute();
+	}
+	return 0;
+}
+
+
+
+#define N 
+ 
+
+
+void preprocess() {
+	
+}
+
+void input() {
+	
+}
+
+void compute() {
+	
+}
